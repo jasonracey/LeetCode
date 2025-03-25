efc5f8ee5a310bdb4a55afacbdcc83b85fc887fa
Jason Racey <jracey@microsoft.com>
2025-03-25 15:19:49 -0700

    Add easy tests

---
+++ LeetCode/Easy.cs
@@ -0,0 +1,104 @@
+﻿namespace LeetCode;
+
+public static class Easy
+{
+    public static int[] TwoSum(int[] nums, int target)
+    {
+        /*
+        Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+        You may assume that each input would have exactly one solution, and you may not use the same element twice.
+        You can return the answer in any order.
+ 
+        Example 1:
+        Input: nums = [2,7,11,15], target = 9
+        Output: [0,1]
+        Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+        
+        Example 2:
+        Input: nums = [3,2,4], target = 6
+        Output: [1,2]
+        
+        Example 3:
+        Input: nums = [3,3], target = 6
+        Output: [0,1]
+ 
+        Constraints:
+        2 <= nums.length <= 104
+        -109 <= nums[i] <= 109
+        -109 <= target <= 109
+        Only one valid answer exists.
+        */
+
+        // two indexes
+        // iterate through array
+        // hold first index
+        // increment second index until sum is met or end of array
+        // increment first index
+        // set second index first + 1
+        // if sum is met set values in return array and return
+        for (var i = 0; i < nums.Length; i++)
+        {
+            for (var j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                    return [i, j];
+            }
+        }
+
+        throw new Exception("No pair of values added up to target value.");
+    }
+
+    public static bool PalindromeNumber(int x)
+    {
+        /*
+        Given an integer x, return true if x is a palindrome, and false otherwise.
+
+        Example 1:
+        Input: x = 121
+        Output: true
+        Explanation: 121 reads as 121 from left to right and from right to left.
+
+        Example 2:
+        Input: x = -121
+        Output: false
+        Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+
+        Example 3:
+        Input: x = 10
+        Output: false
+        Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+        Constraints:
+        -231 <= x <= 231 - 1
+        */
+
+        // negative values are never a palindrome
+        if (x < 0) return false;
+
+        // single digits are always a palindrome
+        if (x >= 0 && x <= 9) return true;
+
+        // we got here if x >= 10
+        var digits = new int[10];
+
+        // put individual digits into an array
+        int index = 0;
+        while (x > 0 && index < digits.Length)
+        {
+            if (x < 10)
+                digits[index++] = x;
+            else
+                digits[index++] = x % 10;
+            x /= 10;
+        }
+
+        // iterate over digits from each end and check palindrome
+        for (int i = 0, j = index - 1; i < j; i++, j--)
+        {
+            if (digits[i] != digits[j])
+                return false;
+        }
+
+        return true;
+    }
+}
---
+++ LeetCodeTests/EasyTests.cs
@@ -0,0 +1,30 @@
+﻿using static LeetCode.Easy;
+
+namespace LeetCodeTests;
+
+[TestClass]
+public class EasyTests
+{
+    [DataTestMethod]
+    [DataRow(new[] { 2,7,11,15 }, 9, new[] { 0, 1})]
+    [DataRow(new[] { 3,2,4 }, 6, new[] { 1, 2})]
+    [DataRow(new[] { 3,3 }, 6, new[] { 0, 1})]
+    public void TwoSumTests(int[] nums, int target, int[] expected)
+    {
+        TwoSum(nums, target).Should().Equal(expected);
+    }
+
+    [DataTestMethod]
+    [DataRow(-121, false)]
+    [DataRow(0, true)]
+    [DataRow(10, false)]
+    [DataRow(11, true)]
+    [DataRow(110, false)]
+    [DataRow(111, true)]
+    [DataRow(121, true)]
+    [DataRow(131000, false)]
+    public void PalindromeNumberTests(int x, bool expected)
+    {
+        PalindromeNumber(x).Should().Be(expected);
+    }
+}
